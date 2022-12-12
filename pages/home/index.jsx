c7309319abba3f377058bdb0c662ed6aa4e2ea96
Antony Gabriel <antonymbsara@gmail.com>
2022-12-12 00:06:27 -0300

    adicionando pagina de login

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     <h1 className="text-6xl text-center w-[949px] font-medium m-auto top-80">Esteja atento (a) aos <span className="text-[#248d22]">prazos dos contratos</span> e torne o seu trabalho <span className="text-[#248d22]">mais produtivo</span>.</h1>
 
     <div>
-      <Link href="/">
+      <Link href="/login">
         <button className="bg-[#248D22] text-white rounded-2xl py-2 px-3 font-bold hover:bg-black mt-8">ENTRAR</button>
       </Link>
   </div>  
